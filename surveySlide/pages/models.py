e5abf3ab760655fc10f310b2862a30d8089c7031
born9507 <born9507@snu.ac.kr>
2020-08-01 16:09:11 +0900

    Update surveyModel

--- surveySlide/pages/models.py
+++ surveySlide/pages/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Survey(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.ForeignKey(User, null=False) #유저가 탈퇴해도 설문지는 남아있도록
+    total_reward = models.IntegerField()
+
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    def update_date(self):
+        self.updated_at = timezone.now()
+        self.save()
+    
+    def __str__(self):
+        return self.title
+
+class Question(models.Model):
+    
+
+class Choice(models.Model):
+
+class Result(models.Model):
