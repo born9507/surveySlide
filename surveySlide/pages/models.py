93dffa1f3ccdd42b0b1f47b253deed730794c9ab
born9507 <born9507@snu.ac.kr>
2020-08-01 16:25:01 +0900

    Update header menu, Add survey model

--- surveySlide/pages/models.py
+++ surveySlide/pages/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Survey(models.Model):
     title = models.CharField(max_length=50)
-    author = models.ForeignKey(User, null=False) #유저가 탈퇴해도 설문지는 남아있도록
+    author = models.ForeignKey(User, null=False, on_delete=models.CASCADE) #유저가 탈퇴하면 누가 만든지는 삭제?
     total_reward = models.IntegerField()
 
     created_at = models.DateTimeField(default=timezone.now)
@@ -18,9 +19,9 @@ class Survey(models.Model):
     def __str__(self):
         return self.title
 
-class Question(models.Model):
+# class Question(models.Model):
     
 
-class Choice(models.Model):
+# class Choice(models.Model):
 
-class Result(models.Model):
+# class Result(models.Model):
