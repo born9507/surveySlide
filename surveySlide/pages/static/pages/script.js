4f01f6c745a15f23cd2488d2a52347316128479d
Jun <sungjun1703@snu.ac.kr>
2020-08-05 15:58:28 +0900

    seongjunNew

--- surveySlide/pages/static/pages/script.js
+++ surveySlide/pages/static/pages/script.js
@@ -1,9 +1,9 @@
 function go_introduction(){
-	document.documentElement.scrollTop = 885;
+	document.documentElement.scrollTop = 970;
 }
 
 function go_price(){
-	document.documentElement.scrollTop = 2260;
+	document.documentElement.scrollTop = 2350;
 }
 
 function go_makeaccount(){
